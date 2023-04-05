516fd93f29ec3617a2157cee7d205b841459e402
euni030 <space_of_record@naver.com>
2023-04-06 02:23:04 +0900

    commit

--- 4. 함수/22. 실습_turtle이벤트함수로 람다함수 사용.py
+++ 4. 함수/22. 실습_turtle이벤트함수로 람다함수 사용.py
@@ -8,16 +8,18 @@ t = turtle.Pen()
 t.speed(0)          # 가장 빠르게 그림
 t.fillcolor("red")  # 빨간색으로 채움
 
-def Draw(x, y):     # 이벤트 처리 함수, (x, y) : 마우스 클릭 좌표
+def Draw(x, y,c):     # 이벤트 처리 함수, (x, y) : 마우스 클릭 좌표
     t.penup()
     t.goto(x, y)    # 마우스 클릭 좌표로 이동
     t.pendown()
+    t.fillcolor(c)
     t.begin_fill()
     t.circle(20)    # 원을 그림
     t.end_fill()
 
 s = turtle.Screen()
-s.onclick(Draw, 1)  # 왼쪽 마우스 버튼 클릭 시 Draw 함수 실행
+s.onclick(lambda x, y: Draw(x,y,"red"),1)  # 왼쪽 마우스 버튼 클릭 시 Draw 함수 실행
+s.onclick(lambda x, y: Draw(x,y,"blue"),3)  # 오른쪽 마우스 버튼 클릭 시 Draw 함수 실행
 s.listen()
 
 turtle.done()       # 이벤트 루프 시작
